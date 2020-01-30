be88545501062b5e11b20b002288d23c00ac6643
Christopher Leja <christopher.leja@gmail.com>
2020-01-30 00:27:36 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -33,6 +33,25 @@ PORTION_1 = {
 def naming_system
   naming_system = {
     label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class",
+        sub_category: {
+          label: "Order",
+          sub_category: {
+            label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category: {
+                label: "Species"
+              }
+            }
+          }
+        }
+      }
+    }
+    
   }
   # Remember:
   #  Kingdom
